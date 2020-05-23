e85cb1602d4b0692d10e441dd64ff1e9d3a90bcf
jheslop25 <john@earthmountain.ca>
2020-05-23 02:17:50 +0000

    one last change didn't get commited for some reason.

--- Tweeter/resources/views/create.blade.php
+++ Tweeter/resources/views/create.blade.php
@@ -4,7 +4,7 @@
             @csrf
                 <input class="form-control w-75" type="text" name="content" placeholder="@ {{Auth::user()->name}} ~ what's on your mind?">
                 <img class="img-fluid" style="width: 60px;" src="{{ url('/logo.png')}}" alt="The Great Ironic Eagle">
-                <label for="myPhoto" class="btn btn-secondary my-2">Select photo
+                <label for="myPhoto" class="btn btn-secondary my-2 mr-2">Select photo
                     <input class="d-none" type="file" id='myPhoto' name="myPhoto">
                 </label>
                 <button class="btn btn-primary m-md-2" type="submit">Publish</button>
